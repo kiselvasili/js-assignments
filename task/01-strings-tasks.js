008537ccc468cf706fffd0a45350e7f97d10c8d6
kiselvasili <kiselvasili@gmail.com>
2016-03-16 15:51:23 +0300

    string task Returns the length of given string.

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -37,7 +37,7 @@ function concatenateStrings(value1, value2) {
  *   ''      => 0
  */
 function getStringLength(value) {
-    throw new Error('Not implemented');
+    return value.length;
 }
 
 /**
